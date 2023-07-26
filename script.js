98af92538f713d0d66f43540ec8066b1c65807fb
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-07-26 16:18:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 //your JS code here. If required.
+const output = document.querySelector('#output');
+const loadingRow = document.createElement('tr');
+const loadingCell = document.createElement('td');
+loadingCell.colSpan = 2;
+loadingCell.textContent = 'Loading...';
+loadingRow.appendChild(loadingCell);
+output.appendChild(loadingRow);
+
+const promises = [];
+const start = performance.now();
+
+for (let i = 0; i < 3; i++) {
+    promises.push(new Promise((resolve) => {
+        const time = Math.random() * 2000 + 1000;
+        setTimeout(() => {
+            resolve([`Promise ${i + 1}`, time / 1000]);
+        }, time);
+    }));
+}
+
+Promise.all(promises).then((results) => {
+    output.removeChild(loadingRow);
+    let totalTime = 0;
+    results.forEach(([name, time]) => {
+        totalTime += time;
+        const row = document.createElement('tr');
+        const nameCell = document.createElement('td');
+        nameCell.textContent = name;
+        row.appendChild(nameCell);
+        const timeCell = document.createElement('td');
+        timeCell.textContent = time.toFixed(3);
+        row.appendChild(timeCell);
+        output.appendChild(row);
+    });
+    const totalRow = document.createElement('tr');
+    const totalNameCell = document.createElement('td');
+    totalNameCell.textContent = 'Total';
+    totalRow.appendChild(totalNameCell);
+    const totalTimeCell = document.createElement('td');
+    totalTimeCell.textContent = ((performance.now() - start) / 1000).toFixed(3);
+    totalRow.appendChild(totalTimeCell);
+    output.appendChild(totalRow);
+});
